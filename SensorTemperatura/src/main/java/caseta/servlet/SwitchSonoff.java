d4a85ae180eebf7d09cefd890b1b56da1a35ce43
746345 <746345@unizar.es>
2021-05-29 18:03:12 +0200

    SwitchSonoff

---
+++ SensorTemperatura/src/main/java/caseta/servlet/SwitchSonoff.java
@@ -0,0 +1,88 @@
+
+package caseta.servlet;
+
+import caseta.ejb.Sonoff;
+import caseta.mqtt.MqttManagerBean;
+import caseta.mqtt.Topic;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *  Iñaki Sánchez   -746345-
+ *  Sistemas y Tecnologías Web
+ *  2021
+ */
+@WebServlet(name = "SwitchSonoff", urlPatterns = {"/switchSonoff"})
+public class SwitchSonoff extends HttpServlet {
+
+    @EJB MqttManagerBean mqttManager;
+    @EJB Sonoff sonoff;
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String comando   = request.getParameter("comando");
+
+        if (comando!=null){
+            mqttManager.publish(Topic.TOPIC_SONOFF_CMND_POWER, comando, false);  
+        }
+        
+        
+        response.sendRedirect(response.encodeURL("index.jsp"));
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
